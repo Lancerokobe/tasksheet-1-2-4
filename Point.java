4bff03d17b0c92e0dcf6daa3fe846bff268394b7
Lancerokobe <141099012+Lancerokobe@users.noreply.github.com>
2024-09-06 00:59:48 +0000

    Taskshee124

--- Point.java
+++ Point.java
@@ -1,1 +1,16 @@
+class Point {
+    private double x;
+    private double y;
 
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public void scale() {
+        this.x = this.x / 2;
+        this.y = this.y / 2;
+    }
+    public void print() {
+        System.out.println("(" + x + "," + y + ")");
+    }
+}
